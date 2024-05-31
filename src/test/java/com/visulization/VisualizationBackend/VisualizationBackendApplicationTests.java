3bee868caed05966b3adb16fc7ca023cf07fcb4d
debnath <dsdebnath4663@gmail.com>
2024-05-31 22:57:59 +0530

    postgres dependency added

--- src/test/java/com/visulization/VisualizationBackend/VisualizationBackendApplicationTests.java
+++ src/test/java/com/visulization/VisualizationBackend/VisualizationBackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.visulization.VisualizationBackend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class VisualizationBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.visulization.VisualizationBackend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class VisualizationBackendApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
