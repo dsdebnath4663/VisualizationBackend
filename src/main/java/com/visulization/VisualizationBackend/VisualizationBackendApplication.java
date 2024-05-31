1d1d931bb9f7907332f0201bb0dc385e4b9c9c78
debnath <dsdebnath4663@gmail.com>
2024-05-31 22:45:08 +0530

    Initial commit

---
+++ src/main/java/com/visulization/VisualizationBackend/VisualizationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.visulization.VisualizationBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualizationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualizationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visulization/VisualizationBackend/VisualizationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visulization.VisualizationBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualizationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
